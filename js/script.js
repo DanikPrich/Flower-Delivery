eb455e91e6f62e292f30db0520bc9b26ee25c7d2
Daniel <Danil.danek@gmail.com>
2023-04-26 11:50:45 +0200

    modal window with logic

--- js/script.js
+++ js/script.js
@@ -1,18 +1,57 @@
 window.addEventListener("load", () => {
-  console.log("page is fully loaded");
 
-  const langSwitch = document.querySelector('.lang')
-  const langBox = document.querySelector('.lang__switch')
+  /* Language switcher */
+  const langBox = document.querySelector('.lang')
+  const langSwitch = document.querySelector('.lang__switch')
   const langOptions = document.querySelector('.lang__options')
 
-  langSwitch.addEventListener('click', () => {
+  
+  langBox.addEventListener('click', () => {
     langOptions.classList.toggle('lang__options_enable')
   })
 
   langOptions.addEventListener('click', (event) => {
     const value = event.target.getAttribute("data-value")
     if (value){
-      langBox.textContent = value;
+      langSwitch.textContent = value;
     }
   })
+
+  /* Modal */
+
+  const modal = document.querySelector('.modal')
+  const modalCard = document.querySelector('.modal-card')
+  const modalTrigger = document.querySelectorAll('[data-modal]')
+  const modalCloseBtn = document.querySelector('[data-modal-close]')
+
+  function openModal() {
+    modal.classList.add('modal__active')
+    document.body.style.overflow = 'hidden'
+  }
+
+  function closeModal() {
+    modal.classList.remove('modal__active')
+    document.body.style.overflow = 'visible'
+  }
+
+  modalTrigger.forEach((elem) => {
+    elem.addEventListener('click', openModal)
+  })
+
+  modalCloseBtn.addEventListener('click', closeModal)
+
+  modal.addEventListener('click', (event) => {
+    console.log(event.target, modal)
+    if (event.target == modal) {
+      closeModal();
+    }
+  })
+
+  document.addEventListener('keydown', (event) => {
+    if (event.code == 'Escape' && modal.classList.contains('modal__active')) {
+      closeModal();
+    }
+  })
+
+  
 });
